47807407de173b258508fcc00634f51d4f2c0857
Cameron Adams <cameron.adams@hey.com>
2022-07-08 01:18:27 -0500

    feat(client): initial setup of client application

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Login from './pages/Login.vue';
+</script>
+
+<template>
+  <Login />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ client/src/pages/Login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const email = ref("");
+  const password = ref("");
+
+  const logEmail = () => console.log(email)
+  const logPassword = () => console.log(password)
+</script>
+
+<template>
+  <input v-model="email" type="email" />
+  <input v-model="password" type="password" />
+</template>
